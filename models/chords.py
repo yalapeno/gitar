bd408fcea85dd5d0b80ec498ff4cda137e2a1bd2
Boran Sahindal <borans@kth.se>
2018-02-26 23:55:30 +0100

    #1 Updated db tables
    Chords and lyrics will be stored in JSON format in the database.

--- models/chords.py
+++ models/chords.py
@@ -1,21 +1,19 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, JSON, ForeignKey
 from gitar_website.database import Base
 
 
-class Genre(Base):
+class Genres(Base):
     """table for allowed genres."""
-    __tablename__ = "genres"
-    id = Column(Integer, primary_key=True)
     name = Column(String(64))  # name of the genre.
+
+    def to_json(self):
+        return dict(name=self.name)
 
     def __init__(self, name=None):
         self.name = name
 
     def __repr__(self):
         return f"<Genre {self.name}>"
-
-    def to_json(self):
-        return dict(name=self.name)
 
     def __eq__(self, other):
         return type(self) is type(other) and self.id == other.id
@@ -24,10 +22,8 @@ class Genre(Base):
         return not self.__eq__(other)
 
 
-class Artist(Base):
+class Artists(Base):
     """table for info about artists."""
-    __tablename__ = "artists"
-    id = Column(Integer, primary_key=True)
     name = Column(String(128))  # name and surname of the artist.
 
     def __init__(self, name=None):
@@ -46,13 +42,23 @@ class Artist(Base):
         return not self.__eq__(other)
 
 
-class GenreReference(Base):
+class GenreReferences(Base):
     """table for music genres of artists. maps artist ids to genre ids in artists and genres tables."""
-    __tablename__ = "genre_reference"
-    id = Column(Integer, primary_key=True) # id for the entry. not sure if it's needed.
     artist_id = Column(Integer, ForeignKey("artists.id"))
     genre_id = Column(Integer, ForeignKey("genres.id"))
 
     def __init__(self, artist_id=None, genre_id=None):
         self.artist_id = artist_id
         self.genre_id = genre_id
+
+
+class Chords(Base):
+    """table for lyrics with chords"""
+    name = Column(String(128))  # name of the song.
+    known_as = Column(String(64))  # alternative name for the song
+    chord_data = JSON  # the chords and other related data to the song.
+
+    def __init__(self, name=None, known_as=None, chord_data=None):
+        self.name = name
+        self.known_as = known_as
+        self.chord_data = chord_data
