1815a65430d50e65b5609d113bcb2c394f7be210
Boran Sahindal <borans@kth.se>
2018-02-26 04:33:20 +0100

    #1 Added more tables
    ''

--- models/chords.py
+++ models/chords.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String, ForeignKey
 from gitar_website.database import Base
 
 
-class Artist(Base):#table for info about artists
-    __tablename__= "artists"
-    id = Column(Integer, primary_key=True)#the id of the artist in the database
-    name = Column(String(100))#name and surname of the artist
+class Genre(Base):
+    """table for allowed genres."""
+    __tablename__ = "genres"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))  # name of the genre.
+
+
+class Artist(Base):
+    """table for info about artists."""
+    __tablename__ = "artists"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))  # name and surname of the artist.
 
     def __init__(self, name=None):
         self.name = name
@@ -24,3 +31,9 @@ class Artist(Base):#table for info about artists
         return not self.__eq__(other)
 
 
+class GenreReference(Base):
+    """table for music genres of artists. maps artist ids to genre ids in corresponding tables."""
+    __tablename__ = "genre_reference"
+    id = Column(Integer, primary_key=True) # id for the entry. not sure if it's needed.
+    artist_id = Column(Integer, ForeignKey("artists.id"))
+    genre_id = Column(Integer, ForeignKey("genres.id"))
