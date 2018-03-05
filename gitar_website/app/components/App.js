47d9d38586f75550eee82aef075239037dccf0ac
abdhas <abdhas@kth.se>
2018-03-05 22:00:31 +0100

    feat: added React environment
    adresses issue #20
    Note that it's not completely done yet, but I have a working React environment now.
    BEfore we merge, can you see if it works on your local computer.

---
+++ gitar_website/app/components/App.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+
+module.exports = App
